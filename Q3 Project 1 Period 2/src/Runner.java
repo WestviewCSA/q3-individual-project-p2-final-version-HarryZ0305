ed47ec245d0a54a374c6d7a9c6f5b8ccc8d65f2c
1950874 <1950874@01-18125.pusd.dom>
2026-02-23 11:47:06 -0800

    Finished map-based input for ALL maps

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Runner {
 
 	public static void main(String[] args) {
-		mapBasedInput("Easy Map 2");
+		mapBasedInput("Medium Map 2");
 
 	}
 	
@@ -18,33 +18,48 @@ public class Runner {
 			int column = Integer.parseInt(map1Scan.next());
 			int level = Integer.parseInt(map1Scan.next());
 			
-			String[][] map = new String[row][column];
+			
+			String[][][] map = new String[level][row][column];
 			
 			System.out.print(row);
 			System.out.print(column);
 			System.out.println(level);
 			
-			int rowCount = 0;
-			int colCount = 0;
-			
-			while(map1Scan.hasNext()) {
-				String result = map1Scan.next();
+			for(int j = 0; j < level; j++) {
+				int rowCount = 0;
+				int colCount = 0;
 				
-				colCount = 0;
-				
-				for(int i = 0; i < result.length(); i++) {
-					map[rowCount][colCount] = result.substring(i, i + 1);
-					colCount++;
-				}
+				while(map1Scan.hasNext()) {
+					String result = map1Scan.next();
+					
+					colCount = 0;
+					
+					for(int i = 0; i < result.length(); i++) {
+						map[j][rowCount][colCount] = result.substring(i, i + 1);
+						colCount++;
+					}
 
-				rowCount++;
-			}
-			
-			for(int i = 0; i < row; i++) {
-				for(int j = 0; j < column; j++) {
-					System.out.println(map[i][j]);
+					rowCount++;
+					
+					if(rowCount == row) {
+						break;
+					}
 				}
 			}
+			
+			
+			for(int z = 0; z < level; z++) {
+				for(int i = 0; i < row; i++) {
+					for(int j = 0; j < column; j++) {
+						System.out.println(map[z][i][j]);
+					}
+				}
+			}
+			
+			
+			System.out.println("Total # of values: "+ (map.length * map[0].length * map[0][0].length));
+			System.out.println("Check whether it is same as Level * Row * Column: " + (level * row * column));
+			
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
