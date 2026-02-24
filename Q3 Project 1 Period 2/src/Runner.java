a5d3cbb15743b8231eb2664a050ba62e9dab9f1c
1950874 <1950874@01-18125.pusd.dom>
2026-02-24 10:54:14 -0800

    Finished coord-based input for ALL maps

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Runner {
 
 	public static void main(String[] args) {
-		mapBasedInput("Medium Map 2");
+		coordBasedInput("Coordinate for Hard 2");
 
 	}
 	
@@ -14,17 +14,20 @@ public class Runner {
 		try {
 			Scanner map1Scan = new Scanner(map1);
 			
+			//The size of the 3D array
 			int row = Integer.parseInt(map1Scan.next());
 			int column = Integer.parseInt(map1Scan.next());
 			int level = Integer.parseInt(map1Scan.next());
 			
-			
+			//The 3D array
 			String[][][] map = new String[level][row][column];
 			
+			//Testing the size
 			System.out.print(row);
 			System.out.print(column);
 			System.out.println(level);
 			
+			//This loop is for each level
 			for(int j = 0; j < level; j++) {
 				int rowCount = 0;
 				int colCount = 0;
@@ -38,16 +41,18 @@ public class Runner {
 						map[j][rowCount][colCount] = result.substring(i, i + 1);
 						colCount++;
 					}
-
+					
+					//New row
 					rowCount++;
 					
+					//After one level end, break to the next level
 					if(rowCount == row) {
 						break;
 					}
 				}
 			}
 			
-			
+			//Testing by printing each value
 			for(int z = 0; z < level; z++) {
 				for(int i = 0; i < row; i++) {
 					for(int j = 0; j < column; j++) {
@@ -56,10 +61,68 @@ public class Runner {
 				}
 			}
 			
-			
+			//Testing the size
 			System.out.println("Total # of values: "+ (map.length * map[0].length * map[0][0].length));
 			System.out.println("Check whether it is same as Level * Row * Column: " + (level * row * column));
 			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void coordBasedInput(String mapName) {
+		File map1 = new File(mapName);
+		try {
+			Scanner map1Scan = new Scanner(map1);
+			
+			//The size of the 3D array
+			int row = Integer.parseInt(map1Scan.next());
+			int column = Integer.parseInt(map1Scan.next());
+			int level = Integer.parseInt(map1Scan.next());
+			
+			//The 3D array
+			String[][][] map = new String[level][row][column];
+			
+			//Testing the size
+			System.out.print(row);
+			System.out.print(column);
+			System.out.println(level);
+			
+			//Put the values into the array
+			while(map1Scan.hasNextLine()) {
+				String symbol = map1Scan.next();
+				int rowWhere = Integer.parseInt(map1Scan.next());
+				int columnWhere = Integer.parseInt(map1Scan.next());
+				int levelWhere = Integer.parseInt(map1Scan.next());
+				
+				map[levelWhere][rowWhere][columnWhere] = symbol;
+			}
+			
+			//Fill the rest with "."
+			for(int z = 0; z < level; z++) {
+				for(int i = 0; i < row; i++) {
+					for(int j = 0; j < column; j++) {
+						if(map[z][i][j] == null) {
+							map[z][i][j] = ".";
+						}
+					}
+				}
+			}
+			
+			
+			//Testing by printing each value
+			for(int z = 0; z < level; z++) {
+				for(int i = 0; i < row; i++) {
+					for(int j = 0; j < column; j++) {
+						System.out.println(map[z][i][j]);
+					}
+				}
+			}
+			
+			//Testing the size
+			System.out.println("Total # of values: "+ (map.length * map[0].length * map[0][0].length));
+			System.out.println("Check whether it is same as Level * Row * Column: " + (level * row * column));
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
